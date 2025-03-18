cdde9feff82b0d5580a68458429bd75bc4375329
Shimizu <shimizulazy@gmail.com>
2025-03-18 21:55:03 +0700

    edit path

--- config/settings.py
+++ config/settings.py
@@ -1,18 +1,27 @@
-import json
 import os
- 
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-CONFIG_FILE = os.path.join(BASE_DIR, "config.json")
+import json
+import sys
+
+def get_config_path():
+    if getattr(sys, 'frozen', False):
+        base_path = os.path.join(os.getenv("APPDATA"), "CoffeeYTDownloader")
+    else:
+        base_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    
+    if not os.path.exists(base_path):
+        os.makedirs(base_path)
+    return os.path.join(base_path, "config.json")
 
 def load_config():
+    config_path = get_config_path()
     default_config = {
         "download_folder": os.path.expanduser("~/Downloads"),
         "format_choice": "Best",
         "language": "English"
     }
-    if os.path.exists(CONFIG_FILE):
+    if os.path.exists(config_path):
         try:
-            with open(CONFIG_FILE, 'r', encoding='utf-8') as f:
+            with open(config_path, 'r', encoding='utf-8') as f:
                 return json.load(f)
         except json.JSONDecodeError as e:
             print(f"Error decoding config.json: {e}. Using default config.")
@@ -20,8 +29,11 @@ def load_config():
     return default_config
 
 def update_config(config):
+    """Lưu config vào file"""
+    config_path = get_config_path()
     try:
-        with open(CONFIG_FILE, 'w', encoding='utf-8') as f:
+        with open(config_path, 'w', encoding='utf-8') as f:
             json.dump(config, f, indent=4)
+        print(f"Config saved to: {config_path}")
     except Exception as e:
         print(f"Error updating config.json: {e}")
